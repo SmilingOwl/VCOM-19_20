6d5979a2e706be93562a3df8a66b679a934fdb88
helenaMontenegro <up201604184@fe.up.pt>
2020-03-15 21:08:12 +0000

    Add blue circle detection

--- src/traffic_light_detection.py
+++ src/traffic_light_detection.py
@@ -2,22 +2,6 @@ import cv2
 import argparse
 import numpy as np
 
-def find_biggest_circle(img):
-    img_canny = cv2.Canny(img, 100, 200)
-    img_blur = cv2.GaussianBlur(img_canny, (5,5), 0)
-    circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1, img_blur.shape[0], param1=200, param2=100, minRadius=0, maxRadius=0)
-
-    max_radius = -1
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
-        for (x, y, r) in circles:
-            if r > max_radius:
-                circle_x = x
-                circle_y = y
-                max_radius = r
-
-    cv2.circle(img, (circle_x, circle_y), max_radius, (0, 255, 0), 4)
-
 def find_circle(img, img_to_show, color):
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     if color == "red":
@@ -26,10 +10,9 @@ def find_circle(img, img_to_show, color):
         img_color = img_red1 + img_red2
     else:
         img_color = cv2.inRange(img_hsv, (100, 150, 0), (140, 255, 255))
-    
     img_canny = cv2.Canny(img_color, 100, 200)
     img_blur = cv2.GaussianBlur(img_canny, (5, 5), 0)
-    circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 2, img_blur.shape[0]/8, param1=200, param2=100, minRadius=0, maxRadius=0)
+    circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1.5, img_blur.shape[0]/8, param1=200, param2=100, minRadius=0, maxRadius=0)
 
     max_radius = -1
     if circles is not None:
