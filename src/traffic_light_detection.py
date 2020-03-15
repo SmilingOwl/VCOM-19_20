6edb91ccb162167680603c300a6d439346fe8995
helenaMontenegro <up201604184@fe.up.pt>
2020-03-15 20:54:31 +0000

    Send clean image to circle function

--- src/traffic_light_detection.py
+++ src/traffic_light_detection.py
@@ -18,17 +18,17 @@ def find_biggest_circle(img):
 
     cv2.circle(img, (circle_x, circle_y), max_radius, (0, 255, 0), 4)
 
-def find_circle(img, color):
-    
-    img_hsv =  cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def find_circle(img, img_to_show, color):
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     if color == "red":
         img_red1 = cv2.inRange(img_hsv, (0, 120, 70), (10, 255, 255))
         img_red2 = cv2.inRange(img_hsv, (170, 120, 70), (180, 255, 255))
         img_color = img_red1 + img_red2
     else:
         img_color = cv2.inRange(img_hsv, (100, 150, 0), (140, 255, 255))
+    
     img_canny = cv2.Canny(img_color, 100, 200)
-    img_blur = cv2.GaussianBlur(img_canny, (5,5), 0)
+    img_blur = cv2.GaussianBlur(img_canny, (5, 5), 0)
     circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 2, img_blur.shape[0]/8, param1=200, param2=100, minRadius=0, maxRadius=0)
 
     max_radius = -1
@@ -40,7 +40,7 @@ def find_circle(img, color):
                 circle_y = y
                 max_radius = r
     if max_radius > -1:
-        cv2.circle(img, (circle_x, circle_y), max_radius, (0, 255, 0), 4)
+        cv2.circle(img_to_show, (circle_x, circle_y), max_radius, (0, 255, 0), 4)
         print(color + " circle")
 
 def main():
@@ -50,11 +50,12 @@ def main():
 
     cv2.namedWindow('image', cv2.WINDOW_NORMAL) # create a normal window
     img = cv2.imread(args["image"], 1) # create image
+    img_to_show = img.copy()
 
-    find_circle(img, "red")
-    find_circle(img, "blue")
+    find_circle(img, img_to_show, "red")
+    find_circle(img, img_to_show, "blue")
 
-    cv2.imshow('Circle', img)
+    cv2.imshow('Circle', img_to_show)
     cv2.waitKey(0) # so that the window doesn't close right away
     cv2.destroyAllWindows() # to destroy all open windows
 
