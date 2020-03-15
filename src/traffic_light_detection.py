0edb8d4c33f9af3a50727d49c275c1128b12d7eb
helenaMontenegro <up201604184@fe.up.pt>
2020-03-15 20:30:23 +0000

    Add some images

--- src/traffic_light_detection.py
+++ src/traffic_light_detection.py
@@ -18,6 +18,32 @@ def find_biggest_circle(img):
 
     cv2.circle(img, (circle_x, circle_y), max_radius, (0, 255, 0), 4)
 
+def find_circle(img, color):
+    
+    img_hsv =  cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    if color == "red":
+        img_red1 = cv2.inRange(img_hsv, (0, 120, 70), (10, 255, 255))
+        img_red2 = cv2.inRange(img_hsv, (170, 120, 70), (180, 255, 255))
+        img_color = img_red1 + img_red2
+    else:
+        img_color = cv2.inRange(img_hsv, (100, 150, 0), (140, 255, 255))
+    #cv2.imshow('Circle', img_color)
+    img_canny = cv2.Canny(img_color, 100, 200)
+    img_blur = cv2.GaussianBlur(img_canny, (5,5), 0)
+    circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1, img_blur.shape[0], param1=200, param2=100, minRadius=0, maxRadius=0)
+
+    max_radius = -1
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            if r > max_radius:
+                circle_x = x
+                circle_y = y
+                max_radius = r
+    if max_radius > -1:
+        cv2.circle(img, (circle_x, circle_y), max_radius, (0, 255, 0), 4)
+        print(color + " circle")
+
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required = False, help = "Path to the image")
@@ -26,7 +52,8 @@ def main():
     cv2.namedWindow('image', cv2.WINDOW_NORMAL) # create a normal window
     img = cv2.imread(args["image"], 1) # create image
 
-    find_biggest_circle(img)
+    find_circle(img, "red")
+    #find_circle(img, "blue")
 
     cv2.imshow('Circle', img)
     cv2.waitKey(0) # so that the window doesn't close right away
