01ea31bc60b011e5004508db48f05292917ff329
helenaMontenegro <up201604184@fe.up.pt>
2020-03-15 21:13:47 +0000

    Comment code for better understanding

--- src/traffic_light_detection.py
+++ src/traffic_light_detection.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 
 def find_circle(img, img_to_show, color):
+    # Obtain black/white image with the wanted colors, according to the string received in color
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     if color == "red":
         img_red1 = cv2.inRange(img_hsv, (0, 120, 70), (10, 255, 255))
@@ -10,10 +11,13 @@ def find_circle(img, img_to_show, color):
         img_color = img_red1 + img_red2
     else:
         img_color = cv2.inRange(img_hsv, (100, 150, 0), (140, 255, 255))
+
+    # Apply Canny Edge Detector to obtain edges, blur image and obtain circles through HoughCircles
     img_canny = cv2.Canny(img_color, 100, 200)
     img_blur = cv2.GaussianBlur(img_canny, (5, 5), 0)
     circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1.5, img_blur.shape[0]/8, param1=200, param2=100, minRadius=0, maxRadius=0)
 
+    # Obtain biggest circle
     max_radius = -1
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
@@ -27,17 +31,20 @@ def find_circle(img, img_to_show, color):
         print(color + " circle")
 
 def main():
+    # Get image from arguments
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required = False, help = "Path to the image")
-    args = vars(ap.parse_args()) # get image from arguments
+    args = vars(ap.parse_args())
 
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL) # create a normal window
+    # Read image
     img = cv2.imread(args["image"], 1) # create image
     img_to_show = img.copy()
 
+    # Find circle
     find_circle(img, img_to_show, "red")
     find_circle(img, img_to_show, "blue")
 
+    # Show result
     cv2.imshow('Circle', img_to_show)
     cv2.waitKey(0) # so that the window doesn't close right away
     cv2.destroyAllWindows() # to destroy all open windows
