787d3999f3062aa0c3710768cf7cfd6076d04621
helenaMontenegro <up201604184@fe.up.pt>
2020-03-25 16:26:29 +0000

    Add blue square detection

--- src/traffic_sign_detection.py
+++ src/traffic_sign_detection.py
@@ -9,7 +9,8 @@ import traffic_sign_detection as traffic
 def find_shapes(img, img_to_show):
     find_circle(img, img_to_show, "red")
     find_circle(img, img_to_show, "blue")
-    find_triangle(img_to_show)
+    find_triangle(img, img_to_show)
+    find_square(img, img_to_show)
 
 def find_circle(img, img_to_show, color):
     
@@ -20,27 +21,22 @@ def find_circle(img, img_to_show, color):
         img_red2 = cv2.inRange(img_hsv, (170, 120, 70), (180, 255, 255))
         img_color = img_red1 + img_red2
     else:
-        img_color = cv2.inRange(img_hsv, (100, 70, 0), (140, 255, 255))
-
-    # Apply Canny Edge Detector to obtain edges, blur image and obtain circles through HoughCircles
+        img_color = cv2.inRange(img_hsv, (100, 150, 0), (140, 255, 255))
+    
     img_canny = cv2.Canny(img_color, 100, 200)
     img_blur = cv2.GaussianBlur(img_canny, (5, 5), 0)
-    circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1.5, img_blur.shape[0]/8, param1=200, param2=100, minRadius=0, maxRadius=0)
+    cv2.imshow("Blur", img_blur)
+    circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1.5, img_blur.shape[0] / 8, param1=200, param2=100, minRadius=0, maxRadius=0)
 
     # Obtain biggest circle
     max_radius = -1
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
         for (x, y, r) in circles:
-            if r > max_radius:
-                circle_x = x
-                circle_y = y
-                max_radius = r
-    if max_radius > -1:
-        cv2.circle(img_to_show, (circle_x, circle_y), max_radius, (0, 255, 0), 4)
-        print(color + " circle")
+            cv2.circle(img_to_show, (x, y), r, (0, 255, 0), 4)
+            print(color + " circle")
 
-def find_triangle(image):
+def find_triangle(image, img_to_show):
     """
     width = 400
     height = 400
@@ -88,7 +84,7 @@ def find_triangle(image):
     cnts = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    sd = ShapeDetector()
+    #sd = ShapeDetector()
 
     # loop over the contours
     for c in cnts:
@@ -109,9 +105,42 @@ def find_triangle(image):
             # multiply the contour (x, y)-coordinates by the resize ratio
                 c = c.astype("float")
                 c =  c.astype("int")
-                cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+                cv2.drawContours(img_to_show, [c], -1, (0, 255, 0), 2)
                 #cv2.putText(image, "triangle", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
             #cv2.imshow("Image", image)
             #cv2.waitKey(0)
             
+def find_square(image, img_to_show):
+
+    blurred = cv2.GaussianBlur(image, (15, 15), 0)
+   
+    # converting to HSV color space
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    img_blue = cv2.inRange(hsv, (100, 123, 0), (140, 255, 255))
+    result_blue = cv2.bitwise_and(image, image, mask=img_blue)
+
+    gray = cv2.cvtColor(result_blue, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 20,255, cv2.THRESH_BINARY)[1]
+
+    # finding contours
+    canny = cv2.Canny(thresh, 100, 200)
+    cnts = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # loop over the contours
+    for c in cnts:
+        M = cv2.moments(c)
+        if(M["m00"] != 0):
+            cX = int((M["m10"] / M["m00"]))
+            cY = int((M["m01"] / M["m00"]))
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+            if len(approx) == 4:
+                print("blue square / rectangle")
+            
+            # multiply the contour (x, y)-coordinates by the resize ratio
+                c = c.astype("float")
+                c =  c.astype("int")
+                cv2.drawContours(img_to_show, [c], -1, (0, 255, 0), 2)
+                #cv2.putText(image, "square", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
